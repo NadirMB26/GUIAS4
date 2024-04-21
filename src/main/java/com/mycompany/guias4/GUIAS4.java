6a2f4a8dc95836cacc5be07659ae5c4bd9b91993
NadirMarsiglia <nadir.marsigliababilonia@unicolombo.edu.co>
2024-04-21 18:52:19 -0500

    primer GUIA4

---
+++ src/main/java/com/mycompany/guias4/GUIAS4.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.guias4;
+
+import igui.Pantallas4;
+
+/**
+ *
+ * @author nadir
+ */
+public class GUIAS4 {
+
+    public static void main(String[] args) {
+      new Pantallas4().setVisible(true);
+    }
+}
